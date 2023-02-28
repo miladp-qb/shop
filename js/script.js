8502e0c8615782ae67f506928b2dfa1af0cc5096
miladpqb <milimaximus1374@gmail.com>
2023-02-28 20:27:00 +0400

    first changes

---
+++ js/script.js
@@ -0,0 +1,45 @@
+window.alert("با سلام و خسته نیاشید . اقای ایران نژاد این طراحی بر روی مانیتور 1080 انجام شده و برای رزولوشن های پایین نیازمند ریسپانسیو هستش که انجام نشده ")
+
+
+let userMenuBtn = document.querySelector('.user-menu-btn')
+
+
+
+userMenuBtn.addEventListener('click' , function(e){
+    e.preventDefault();
+
+    let userMenu = document.querySelector('.user-menu')
+
+    userMenu.classList.toggle('none')
+})
+
+
+
+
+let scrollBox = document.querySelector('.ssi-items-box');
+
+if(scrollBox){
+
+    let itemNext = document.querySelector('.item-next');
+    let itemPrev = document.querySelector('.item-prev');
+
+
+    itemNext.addEventListener('click' , function(e){
+        e.preventDefault();
+
+        scrollBox.scrollLeft -= 200
+        
+    })
+
+    itemPrev.addEventListener('click' , function(e){
+        e.preventDefault();
+
+        scrollBox.scrollLeft += 200
+    })
+
+}
+
+console.log(scrollBox.scrollLeft)
+
+
+
